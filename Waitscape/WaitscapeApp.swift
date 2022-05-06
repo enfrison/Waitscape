bf6e9b369614f7e50f8a2c3be8d96b791ef65663
enfrison <98891923+enfrison@users.noreply.github.com>
2022-05-06 10:10:41 -0400

    Initial Commit

---
+++ Waitscape/WaitscapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaitscapeApp.swift
+//  Waitscape
+//
+//  Created by Erika Frison on 5/6/22.
+//
+
+import SwiftUI
+
+@main
+struct WaitscapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
