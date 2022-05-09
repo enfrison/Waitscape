60299c67a713604d3bd93ec631f62a26f259fa75
enfrison <98891923+enfrison@users.noreply.github.com>
2022-05-09 11:28:25 -0400

    I was able to add the Arc under the search bar.

--- Waitscape/ContentView.swift
+++ Waitscape/ContentView.swift
@@ -7,20 +7,48 @@
 
 import SwiftUI
 
+struct Arc: Shape {
+var waitTime: Double
+//    let startAngle: Angle
+//    let endAngle: Angle
+//    let clockwise: Bool
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: Angle(degrees: -240), endAngle: Angle(degrees: Double(-240.0 + (waitTime * 300 / 120))), clockwise: false)
+        
+        return path
+    }
+}
+
 struct ContentView: View {
+    @State private var waitTime: Double = 15
     @State private var searchAirports = ""
     var body: some View {
         VStack{
+
+            Image("Waitscape Logo")
+                .resizable()
+                .scaledToFit()
+            NavigationView{
+                
+                ZStack{
+                    Text("Detroit Metro Airport")
+                      .font(.headline)
+                Arc(waitTime: 120)
+                    .stroke(.blue, lineWidth: 12)
+
+                    Arc(waitTime: waitTime)
+                        .stroke(.gray, lineWidth:12)
+           
             
-         Image("Waitscape Logo")
-            .resizable()
-            .scaledToFit()
-        NavigationView{
-            Text("Detroit Metro Airport")
-                .font(.headline)
+                }
+                .padding()
+            }
+            
         }
         .searchable(text: $searchAirports)
-        }
     }
 }
 
