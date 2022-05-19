0eb5bee5445ea410de2f56b39784ca0c0ff671df
enfrison <98891923+enfrison@users.noreply.github.com>
2022-05-19 10:56:16 -0400

    I had to completely customize the search bar because the one set with in the navigation bar wouldn't move up or down on the screen. I commented out the rest of the items we weren't using.

--- Waitscape/ContentView.swift
+++ Waitscape/ContentView.swift
@@ -32,27 +32,68 @@ struct ContentView: View {
     @State private var airportStatus = AirportStatus(rightnow_description: "Empty", city: "Nothing", state: "Nothing", code: "Nothing")
     @State private var waitTime: Double = 15
     @State private var searchAirports = ""
+    @State var isSearching = false
+    
     var body: some View {
         VStack{
 
             Image("Waitscape Logo")
                 .resizable()
-                .padding([.top, .leading, .bottom])
+                .padding([.leading, .bottom])
                 .frame(width: 220.0, height: 150.0)
                 .scaledToFit()
                 .offset(x: -105, y: -5)
-            NavigationView{
+            HStack{
+            HStack{
+                TextField("Search Airports", text: $searchAirports)
+                    .padding(.leading, 24)
+            }
+            .padding()
+            .background(Color(.systemGray5))
+            .cornerRadius(12)
+            .padding(.horizontal)
+            .onTapGesture(perform: {
+                isSearching = true
+            })
+            .overlay(
+                HStack{
+                   Image(systemName: "magnifyingglass")
+                    Spacer()
+                if isSearching {
+                    Button(action: { searchAirports = "" }, label: {
+                        Image(systemName: "xmark.circle.fill")
+                            .padding(.vertical)
+                    })
+                   
+                    }
+                }.padding(.horizontal, 32)
+                    .foregroundColor(.gray)
+            )
+                if isSearching{
+                Button(action: { isSearching = false
+                    searchAirports = ""
+                }, label: { Text("Cancel")
+                        .padding(.trailing)
+                        .padding(.leading, -12)
+                })
+                    .transition(.move(edge: .trailing))
+                    
+            }
+            }
+            
+            
+//            NavigationView{
         
              
-                VStack {
-//                    HStack {
-//                        Text(airportStatus.city)
-//                            .font(.largeTitle)
-//
-//                        Text(airportStatus.state)
-//                            .font(.largeTitle)
-//                    }
-                    }
+                //      VStack {
+                    //                    HStack {
+                    //                        Text(airportStatus.city)
+                    //                            .font(.largeTitle)
+                    //
+                    //                        Text(airportStatus.state)
+                    //                            .font(.largeTitle)
+                    //                    }
+  //
 //                    Text(airportStatus.code)
 //                        .font(.title2)
 //                        .padding()
@@ -60,8 +101,9 @@ struct ContentView: View {
                     
                     //Text(airportStatus.rightnow_description)
                     //Text("Wait time at :")
-                .padding()
-                
+          //      .padding()
+            .padding(.vertical, -15)
+            Spacer()
                     ZStack {
                         VStack{
                             Text(airportStatus.code)
@@ -79,7 +121,8 @@ struct ContentView: View {
                             .stroke(Color("Waitscape Orange"), lineWidth:12)
                
                     }
-                     .frame(width: 300, height: 300)
+                    .padding(.bottom, 100.0)
+                    .frame(width: 300, height: 300)
                 .task {
                     guard let url = URL(string: "https://www.tsawaittimes.com/api/airport/bpmLZd3ywayMaJfSJHIjLGxzxOZlpzA9/DTW") else {
                         print("Invalid URL")
@@ -99,9 +142,10 @@ struct ContentView: View {
                  
                     
                   
-            }
-            .searchable(text: $searchAirports)
-            .navigationBarBackButtonHidden(true)
+//            }
+//            .searchable(text: $searchAirports)
+            
+        
            
             }
       
